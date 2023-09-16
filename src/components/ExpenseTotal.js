d53ecf54d1d0adeaca4382e5f7ad9ee926ef637f
vansheet <144224686+vansheet@users.noreply.github.com>
2023-09-16 13:09:25 +0530

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const ExpenseTotal = () => {
-    const { expenses, currency } = useContext(AppContext);
+    const { expenses, dispatch, currency } = useContext(AppContext);
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
     }, 0);
