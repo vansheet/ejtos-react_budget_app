3c99bf6b1091324666c139bbb68b6f74ff2a6f4b
vansheet <144224686+vansheet@users.noreply.github.com>
2023-09-16 13:22:49 +0530

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -59,7 +59,7 @@ const AllocationForm = (props) => {
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
 
-                    <span style={{ "position":"relative","right":"-20px"}}> £ </span>
+                    <span style={{ "position":"relative","right":"-20px"}}> {currency} </span>
                     <input
                         required='required'
                         type='number'
